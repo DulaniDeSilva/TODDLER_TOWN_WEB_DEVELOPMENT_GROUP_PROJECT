cd2d8a74e2756157799b8e7a9dfbddb91588aae4
DulaniDeSilva <addsdesilva8@gmail.com>
2023-10-25 10:47:23 +0530

    Modify src, add component, pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+// IMPORTS NILMI
+
+// IMPORTS CHAMITHTHREE
+
+
+// IMPORTS CHALANI
+
+
+// IMPORTS DULANI
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+
+    {/* ADDING ROUTER PATHS */}
+    {/* NILMI ROUTER PATHS */}
+
+
+
+
+
+    {/* CHALANI ROUTER PATHS */}
+
+
+
+
+
+    {/* CHAMITHTHREE ROUTER PATHS */}
+
+
+
+
+
+    {/* DULANI ROUTER PATHS */}
+
+ 
     </div>
   );
 }
