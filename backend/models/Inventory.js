654a871e40393e7008a4105114d790e360c7a355
DulaniDeSilva <addsdesilva8@gmail.com>
2023-11-22 20:56:45 +0530

    Create Inventory database

---
+++ backend/models/Inventory.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const InventorySchema = new Schema({
+    itemName: {type:String, required:true},
+    description: {type:String},
+    quantity: {type:Number, required:true},
+    date: {type:Date, default:Date.now},
+}
+    
+);
+
+
+//creating model
+const InventoryModel = mongoose.model("Inventory",InventorySchema);
+
+module.exports = InventoryModel;
---
+++ backend/routes/inventory.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+let Inventory = require("../models/Inventory");
+
+//crating a item
+
+
+
+module.exports = router;
